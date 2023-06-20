c22715ba9d1436094c0d511916f16313225a1f27
shiva kumar <shivakumar@shivas-MacBook-Air.local>
2023-06-20 19:54:18 +0530

    up

--- src/components/hero.js
+++ src/components/hero.js
@@ -20,6 +20,7 @@ import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 import EuroIcon from "@mui/icons-material/Euro";
 import "./style.css";
+import Web3 from "web3"; // Import web3.js library
 import {
   Busd_Address,
   Coin_Abi,
@@ -27,9 +28,6 @@ import {
   Ico_Address,
   Usdt_Address,
 } from "../blockchain/config";
-import { readContract } from "@wagmi/core";
-import { prepareWriteContract, writeContract } from "@wagmi/core";
-import { fetchBalance } from "@wagmi/core";
 
 function Hero(props) {
   const theme = useTheme();
@@ -47,10 +45,25 @@ function Hero(props) {
     setValues((previous) => ({ ...previous, [id]: value }));
   };
 
+  const web3 = new Web3(window.ethereum); // Initialize web3 object
+
+  const approve = async (tokenAddress, spender, amount) => {
+    const contract = new web3.eth.Contract(Coin_Abi, tokenAddress);
+    const accounts = await web3.eth.getAccounts();
+    await contract.methods
+      .approve(spender, amount)
+      .send({ from: accounts[0] });
+  };
+
+  const buyToken = async (tokenAddress, amount) => {
+    const contract = new web3.eth.Contract(Ico_Abi, Ico_Address);
+    const accounts = await web3.eth.getAccounts();
+    await contract.methods.BuyToken_busd(amount).send({ from: accounts[0] });
+  };
+
   const ButtonControler = async () => {
     try {
       if (address !== undefined) {
-        // console.log(User_Wallet)
         if (values.selectedCurrency === "USD") {
           if (values.enteredValue === 0) {
             alert("Coin Amount Not selected");
@@ -75,234 +88,199 @@ function Hero(props) {
 
   const Buy_Usdt = async () => {
     try {
-      const approve = await prepareWriteContract({
-        address: Usdt_Address,
-        abi: Coin_Abi,
-        functionName: "approve",
-        args: [
-          Ico_Address,
-          BigInt(values.enteredValue * 1000000000000000000)
-        ] /* global BigInt */,
-      });
-      await writeContract(approve);
-      const Buy = await prepareWriteContract({
-        address: "0xecb504d39723b0be0e3a9aa33d646642d1051ee1",
-        abi: Ico_Abi,
-        functionName: "BuyToken_Usdt_referal",
-        args: [values.enteredValue, 0],
-      });
-      await writeContract(Buy);
+      await approve(Usdt_Address, Ico_Address, values.enteredValue);
+      await buyToken(Usdt_Address, values.enteredValue);
     } catch (error) {
-      alert("error in buy function");
+      alert("Error in buy function");
       console.log(error);
     }
   };
 
   const Buy_Busd = async () => {
     try {
-      const approve = await prepareWriteContract({
-        address: Usdt_Address,
-        abi: Coin_Abi,
-        functionName: "approve",
-        args: [Ico_Address, BigInt(values.enteredValue * 1000000000000000000)],
-      });
-      await writeContract(approve);
-      const Buy = await prepareWriteContract({
-        address: Ico_Address,
-        abi: Ico_Abi,
-        functionName: "BuyToken_Busd_referal",
-        args: [values.enteredValue, 0],
-      });
-      await writeContract(Buy);
+      await approve(Busd_Address, Ico_Address, values.enteredValue);
+      await buyToken(Busd_Address, values.enteredValue);
     } catch (error) {
       console.log(error);
-      alert("error in buy function");
     }
   };
 
   return (
     <section className="hero">
-      <Grid container spacing={10} sx={{ height: "100%" }}>
-        <Grid item xs={12} md={6}>
-          <Stack spacing={4} justifyContent={"center"} height={"100%"}>
-            <Box className="content">
-              <Typography gutterBottom variant="h2" fontWeight={"bold"}>
-                {props.heading}
-              </Typography>
-              <Typography gutterBottom variant="h2" fontWeight={"bold"}>
-                {props.heading}
-              </Typography>
-            </Box>
-            <Typography variant="caption" color={theme.palette.secondary.main}>
-              {props.subHeading}
+    <Grid container spacing={10} sx={{ height: "100%" }}>
+      <Grid item xs={12} md={6}>
+        <Stack spacing={4} justifyContent={"center"} height={"100%"}>
+          <Box className="content">
+            <Typography gutterBottom variant="h2" fontWeight={"bold"}>
+              {props.heading}
             </Typography>
-            <Stack direction="row" spacing={1}>
-              <IconButton variant="contained" color="secondary">
-                <CurrencyBitcoinIcon />
-              </IconButton>
-              <IconButton variant="contained" color="secondary">
-                <CurrencyExchangeIcon />
-              </IconButton>
-              <IconButton variant="contained" color="secondary">
-                <CurrencyRupeeIcon />
-              </IconButton>
-              <IconButton variant="contained" color="secondary">
-                <EuroIcon />
-              </IconButton>
-            </Stack>
+            <Typography gutterBottom variant="h2" fontWeight={"bold"}>
+              {props.heading}
+            </Typography>
+          </Box>
+          <Typography variant="caption" color={theme.palette.secondary.main}>
+            {props.subHeading}
+          </Typography>
+          <Stack direction="row" spacing={1}>
+            <IconButton variant="contained" color="secondary">
+              <CurrencyBitcoinIcon />
+            </IconButton>
+            <IconButton variant="contained" color="secondary">
+              <CurrencyExchangeIcon />
+            </IconButton>
+            <IconButton variant="contained" color="secondary">
+              <CurrencyRupeeIcon />
+            </IconButton>
+            <IconButton variant="contained" color="secondary">
+              <EuroIcon />
+            </IconButton>
           </Stack>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={6}
+        </Stack>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Card
+          variant="elevation"
+          elevation={5}
+          className="coinCard"
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
+            [theme.breakpoints.down("md")]: {
+              width: "auto !important",
+            },
           }}
         >
-          <Card
-            variant="elevation"
-            elevation={5}
-            className="coinCard"
-            sx={{
-              [theme.breakpoints.down("md")]: {
-                width: "auto !important",
-              },
-            }}
-          >
-            <CardHeader
-              sx={{ textAlign: "center" }}
-              title={
+          <CardHeader
+            sx={{ textAlign: "center" }}
+            title={
+              <Typography
+                variant="h4"
+                gutterBottom
+                fontWeight={"bold"}
+                color={theme.palette.warning.main}
+              >
+                DuCoin
+              </Typography>
+            }
+            // subheader={
+            //   <Typography variant="body2">A TOKEN TO THE DUVERSE</Typography>
+            // }
+          />
+          <CardContent>
+            <Stack direction="row" spacing={2}>
+              <Box sx={{ flex: 1 }}>
                 <Typography
-                  variant="h4"
-                  gutterBottom
+                  variant="h6"
                   fontWeight={"bold"}
                   color={theme.palette.warning.main}
                 >
-                  DuCoin
+                  $DUCOIN Sold
                 </Typography>
-              }
-              // subheader={
-              //   <Typography variant="body2">A TOKEN TO THE DUVERSE</Typography>
-              // }
-            />
-            <CardContent>
-              <Stack direction="row" spacing={2}>
-                <Box sx={{ flex: 1 }}>
-                  <Typography
-                    variant="h6"
-                    fontWeight={"bold"}
-                    color={theme.palette.warning.main}
-                  >
-                    $DUCOIN Sold
-                  </Typography>
-                  <Box className="wrapperBox">
-                    <Typography>{values.coinsold}</Typography>
-                  </Box>
+                <Box className="wrapperBox">
+                  <Typography>{values.coinsold}</Typography>
                 </Box>
-                <Box sx={{ flex: 1 }}>
-                  <Typography
-                    variant="h6"
-                    fontWeight={"bold"}
-                    color={theme.palette.warning.main}
-                  >
-                    $DUCOIN Left
-                  </Typography>
-                  <Box className="wrapperBox">
-                    <Typography>{values.coinleft}</Typography>
-                  </Box>
-                </Box>
-              </Stack>
-            </CardContent>
-            <CardContent>
-              <Stack direction="row" spacing={2}>
-                <TextField
-                  hiddenLabel
-                  variant="filled"
-                  size="medium"
-                  fullWidth
-                  id="enteredValue"
-                  value={values.enteredValue}
-                  onChange={handleChange}
-                  type="number"
-                  className="textInput"
-                  sx={{
-                    background: theme.palette.secondary.main,
-                    color: theme.palette.text.primary,
-                    borderRadius: 2,
-                  }}
-                  InputProps={{
-                    style: { padding: 0 },
-                    endAdornment: (
-                      <InputAdornment sx={{ mr: 0 }} position="start">
-                        <TextField
-                          select
-                          id="selectedCurrency"
-                          value={values.selectedCurrency}
-                          onChange={handleChange}
-                        >
-                          {[
-                            { label: "$USDT", value: "USDT" },
-                            { label: "$BUSD", value: "BUSD" },
-                          ].map((option, i) => (
-                            <MenuItem key={i} value={option.value}>
-                              {option.label}
-                            </MenuItem>
-                          ))}
-                        </TextField>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-                <TextField
-                  hiddenLabel
-                  disabled
-                  variant="filled"
-                  size="medium"
-                  fullWidth
-                  value={values.enteredValue * 12}
-                  className="textInput"
-                  sx={{
-                    background: theme.palette.secondary.main,
-                    color: theme.palette.text.primary,
-                    borderRadius: 2,
-                  }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="start">
-                        <Typography>$COIN</Typography>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Stack>
-            </CardContent>
-            <CardContent>
-              <center>
-                <Button
-                  variant="contained"
-                  color="warning"
-                  size="large"
-                  onClick={ButtonControler}
+              </Box>
+              <Box sx={{ flex: 1 }}>
+                <Typography
+                  variant="h6"
+                  fontWeight={"bold"}
+                  color={theme.palette.warning.main}
                 >
-                  Buy Coin
-                </Button>
-              </center>
-            </CardContent>
-          </Card>
-        </Grid>
+                  $DUCOIN Left
+                </Typography>
+                <Box className="wrapperBox">
+                  <Typography>{values.coinleft}</Typography>
+                </Box>
+              </Box>
+            </Stack>
+          </CardContent>
+          <CardContent>
+            <Stack direction="row" spacing={2}>
+              <TextField
+                hiddenLabel
+                variant="filled"
+                size="medium"
+                fullWidth
+                id="enteredValue"
+                value={values.enteredValue}
+                onChange={handleChange}
+                type="number"
+                className="textInput"
+                sx={{
+                  background: theme.palette.secondary.main,
+                  color: theme.palette.text.primary,
+                  borderRadius: 2,
+                }}
+                InputProps={{
+                  style: { padding: 0 },
+                  endAdornment: (
+                    <InputAdornment sx={{ mr: 0 }} position="start">
+                      <TextField
+                        select
+                        id="selectedCurrency"
+                        value={values.selectedCurrency}
+                        onChange={handleChange}
+                      >
+                        {[
+                          { label: "$USDT", value: "USDT" },
+                          { label: "$BUSD", value: "BUSD" },
+                        ].map((option, i) => (
+                          <MenuItem key={i} value={option.value}>
+                            {option.label}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                hiddenLabel
+                disabled
+                variant="filled"
+                size="medium"
+                fullWidth
+                value={values.enteredValue * 12}
+                className="textInput"
+                sx={{
+                  background: theme.palette.secondary.main,
+                  color: theme.palette.text.primary,
+                  borderRadius: 2,
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <Typography>$COIN</Typography>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Stack>
+          </CardContent>
+          <CardContent>
+            <center>
+              <Button
+                variant="contained"
+                color="warning"
+                size="large"
+                onClick={ButtonControler}
+              >
+                Buy Coin
+              </Button>
+            </center>
+          </CardContent>
+        </Card>
       </Grid>
+    </Grid>
     </section>
-  );
-}
+  )}
 
-Hero.defaultProps = {
-  heading: "BUY",
-  heading: "DU-COIN.",
-  subHeading: `Join the DU-COIN revolution and unlock a new era of decentralized finance!`,
-};
-
-export default Hero;
+  export default Hero
